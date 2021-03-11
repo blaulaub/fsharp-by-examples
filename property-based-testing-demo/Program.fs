f9434a92f0fa8d64530ba1b4e09b11de8f78a56b
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-11 23:29:29 +0100

    add assertion tests

--- property-based-testing-demo/Program.fs
+++ property-based-testing-demo/Program.fs
@@ -1,13 +1,34 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// see https://www.youtube.com/watch?v=IYzDFHx6QPY
 
-open System
+open Expecto
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+// some extremly complicated (and possibly bogus) implementation
+let plus x y =
+    match (x, y) with
+    | (14, 7) -> 21
+    | _ -> 4
+
+
+// tests for the extremly complicated implementation
+module Tests =
+
+    /// <summary>abstraction of classical assertion testing</summary>
+    /// <remarks>Some given input (could be a corner case) is feed
+    /// into the testee, and the outcome is compared to an expected
+    /// result.</remarks>
+    let assertionTest input testee expected =
+        let actual = input |> testee
+        Expect.equal actual expected "actual result does not match expected"
+
+    /// <summary>Testsuite of classical assertion tests.</summary>
+    [<Tests>]
+    let assertionTests =
+        testList "AssertionTests" [
+            test "1 plus 3 gives 4"   { assertionTest (1, 3)  (fun (x, y) -> plus x y) (4)  }
+            test "14 plus 7 gives 21" { assertionTest (14, 7) (fun (x, y) -> plus x y) (21) }
+            // and we can write much much more...
+        ]
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main args =
+    runTestsInAssemblyWithCLIArgs [] args
