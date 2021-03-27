eb0bbda0acbd44956e68cf5ccac9fd11733dc444
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-27 13:19:23 +0100

    remove wordy comment

--- sudoku-solver/SudokuBoard.fs
+++ sudoku-solver/SudokuBoard.fs
@@ -1,14 +1,5 @@
 namespace Ch.PatchCode.SudokuSolver
 
-/// <summary>
-/// Represents the content of a Sudoku board, solved or incomplete.
-/// </summary>
-/// <remarks>
-///  Some of the fields contain an integer value, some of them are empty.
-/// </remarks>
-/// <remarks>
-/// Arrays and subarrays are expected to have the same size.
-/// </remarks>
 type SudokuBoard = int option array array
 
 module SudokuBoard =
