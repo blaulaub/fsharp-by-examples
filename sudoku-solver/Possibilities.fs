6738ac5267a343c792d1d82511ec434e7d5ad8aa
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-27 13:57:28 +0100

    make possibilities more general

--- sudoku-solver/Possibilities.fs
+++ sudoku-solver/Possibilities.fs
@@ -5,13 +5,14 @@ type Possibilities = int list array array
 module Possibilities =
 
     /// <summary>
-    /// Derives initial options from a given <see cref="Sudoku"/> setup.
+    /// Derives initial options from a given <see cref="Board"/> setup.
     /// </summary>
-    let fromBoard (sudoku: Board): Possibilities =
-        [| for row in sudoku ->
+    let fromBoard (board: Board): Possibilities =
+        let total = board.Length
+        [| for row in board ->
             [| for field in row ->
                 match field with
                 | Some num -> [ num ]
-                | None -> [1..9]
+                | None -> [1..total]
              |]
         |]
