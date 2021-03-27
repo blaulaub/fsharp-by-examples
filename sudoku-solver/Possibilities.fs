0f93560cabff54e290948a6aa8f26021a8ca0ae5
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-27 15:06:46 +0100

    more spaces

--- sudoku-solver/Possibilities.fs
+++ sudoku-solver/Possibilities.fs
@@ -8,7 +8,9 @@ module Possibilities =
     /// Derives initial options from a given <see cref="Board"/> setup.
     /// </summary>
     let fromBoard (board: Board): Possibilities =
+
         let total = board.Length
+
         [| for row in board ->
             [| for field in row ->
                 match field with
