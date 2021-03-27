e3f32b9d7611dabdae94db1f876087e249e3046a
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-27 13:53:20 +0100

    comment

--- sudoku-solver/Board.fs
+++ sudoku-solver/Board.fs
@@ -1,9 +1,17 @@
 namespace Ch.PatchCode.SudokuSolver
 
+/// <summary>
+/// Board of fields that make up a Sudoku board.
+/// </summary>
 type Board = int option array array
 
 module Board =
 
+    /// <summary>
+    /// An empty board.
+    /// </summary>
+    /// <param name="superRows">number of sub-blocks per row</param>
+    /// <param name="superColumns">number of sub-blocks per column</param>
     let empty (superRows: int) (superColumns: int): Board =
 
         let total = superRows * superColumns
--- sudoku-solver/RuleGroup.fs
+++ sudoku-solver/RuleGroup.fs
@@ -1,23 +1,34 @@
 namespace Ch.PatchCode.SudokuSolver
 
+/// <summary>
+/// Group of fields for which the Sudoku rule (each value once) should apply.
+/// </summary>
 type RuleGroup = (int*int) seq
 
 module RuleGroup =
 
+    /// <summary>
+    /// All groups where the Sudoku rule (unique value) should apply.
+    /// </summary>
+    /// <param name="superRows">number of sub-blocks per row</param>
+    /// <param name="superColumns">number of sub-blocks per column</param>
     let groups (superRows: int) (superColumns: int): RuleGroup seq = seq {
 
         let total = superRows * superColumns
 
-        for row in 0..8 -> seq { for col in 0..(total-1) -> (row, col) }
+        for row in 0..8 ->
+            seq { for col in 0..(total-1) -> (row, col) }
 
-        for col in 0..8 -> seq { for row in 0..(total-1) -> (row, col) }
+        for col in 0..8 ->
+            seq { for row in 0..(total-1) -> (row, col) }
 
         let subRows = superColumns
         let subCols = superRows
         for supRow in 0..(superRows-1) do
-        for supCol in 0..(superColumns-1) -> seq {
-            for subRow in 0..(subRows-1) do
-            for subCol in 0..(subCols-1) ->
-                (supRow*3+subRow, supCol*3+subCol)
+        for supCol in 0..(superColumns-1) ->
+            seq {
+                for subRow in 0..(subRows-1) do
+                for subCol in 0..(subCols-1) ->
+                    (supRow*3+subRow, supCol*3+subCol)
+            }
         }
-    }
