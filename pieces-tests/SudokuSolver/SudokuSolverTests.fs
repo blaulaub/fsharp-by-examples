377b131f469d1c6c84101211c2dea6b01960982b
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-27 22:23:07 +0100

    added breaking problem

--- pieces-tests/SudokuSolver/SudokuSolverTests.fs
+++ pieces-tests/SudokuSolver/SudokuSolverTests.fs
@@ -30,6 +30,20 @@ let difficultSudokuSolution =
         [| Some 7; Some 2; Some 1; Some 8; Some 6; Some 4; Some 3; Some 9; Some 5 |]
     |]
 
+/// Sample data for another difficult Sudoku
+let difficultSudoku2 =
+    [|
+        [| None  ; Some 4; None  ; None  ; Some 1; None  ; None  ; Some 9; None   |];
+        [| Some 2; None  ; None  ; Some 7; None  ; Some 8; None  ; None  ; Some 1 |];
+        [| None  ; None  ; None  ; Some 3; None  ; Some 4; None  ; None  ; None   |];
+        [| None  ; Some 6; Some 5; None  ; None  ; None  ; Some 7; Some 4; None   |];
+        [| Some 4; None  ; None  ; None  ; None  ; None  ; None  ; None  ; Some 3 |];
+        [| None  ; Some 8; Some 3; None  ; None  ; None  ; Some 9; Some 5; None   |];
+        [| None  ; None  ; None  ; Some 8; None  ; Some 3; None  ; None  ; None   |];
+        [| Some 8; None  ; None  ; Some 5; None  ; Some 9; None  ; None  ; Some 4 |];
+        [| None  ; Some 3; None  ; None  ; Some 4; None  ; None  ; Some 6; None   |]
+    |]
+
 // Sample data for an easy Sudoku
 let easySudoku =
     [|
@@ -122,7 +136,7 @@ let tests =
 
         test "try solve some board" {
             let initialState =
-                SolverState.fromBoard difficultSudoku
+                SolverState.fromBoard difficultSudoku2
 
             initialState
             |> SolverState.solveWithPreAction (fun state ->
