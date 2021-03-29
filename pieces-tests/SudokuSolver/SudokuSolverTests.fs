c42cb11a8d87fc15b2e5316792312430bc1421d4
Christian Fuchs <kontaktierbar@gmail.com>
2021-03-30 00:02:17 +0200

    added inventor

--- pieces-tests/SudokuSolver/SudokuSolverTests.fs
+++ pieces-tests/SudokuSolver/SudokuSolverTests.fs
@@ -167,55 +167,10 @@ let tests =
 
         test "try invent some board" {
 
-
-            let apply next0 (row, col, n) =
-                next0
-                |> SolverState.applySingularOptionToState { Row = row; Col = col; Value = n}
-
-
-            let nextStateAndPick (rnd: int -> int) state =
-
-                let next0 =
-                    state
-                    |> SolverState.solve
-
-                let undet = [|
-                    for row in 0..8 do
-                    for col in 0..8 do
-                    if next0.Options.[row].[col].Length > 0
-                    then yield (row, col)
-                |]
-
-                if undet.Length > 0
-                then
-                    let (row, col) = undet.[rnd undet.Length]
-                    let l = rnd next0.Options.[row].[col].Length
-                    let n = next0.Options.[row].[col].[l]
-                    (next0, Some (row, col, n))
-                else
-                    (next0, None)
-
-
-            let rec getHints (rnd: int -> int) state = seq {
-                match nextStateAndPick rnd state with
-                | (next0, Some pick) ->
-                    yield pick
-                    yield! getHints rnd (apply next0 pick)
-                | (_, None) -> ()
-            }
-
-
-            let initialState = Board.empty 3 3 |> SolverState.fromBoard
             let rnd = System.Random()
             let nextRandom upperEx = rnd.Next(upperEx)
 
-            let initialState () = Board.empty 3 3 |> SolverState.fromBoard
-
-            let board =
-                initialState ()
-                |> getHints nextRandom
-                |> Seq.fold apply (initialState ())
-                |> (fun state -> state.Board)
+            let board = Inventor.invent 3 3 nextRandom
 
             let hintCount (board: Board) =
                 board
---
+++ sudoku-solver/Inventor.fs
@@ -0,0 +1,47 @@
+namespace Ch.PatchCode.SudokuSolver
+
+module Inventor =
+
+    let private apply next0 (row, col, n) =
+        next0
+        |> SolverState.applySingularOptionToState { Row = row; Col = col; Value = n}
+
+
+    let private nextStateAndPick (rnd: int -> int) state =
+
+        let next0 =
+            state
+            |> SolverState.solve
+
+        let undet = [|
+            for row in 0..8 do
+            for col in 0..8 do
+            if next0.Options.[row].[col].Length > 0
+            then yield (row, col)
+        |]
+
+        if undet.Length > 0
+        then
+            let (row, col) = undet.[rnd undet.Length]
+            let l = rnd next0.Options.[row].[col].Length
+            let n = next0.Options.[row].[col].[l]
+            (next0, Some (row, col, n))
+        else
+            (next0, None)
+
+    let rec private getHints (rnd: int -> int) state = seq {
+        match nextStateAndPick rnd state with
+        | (next0, Some pick) ->
+            yield pick
+            yield! getHints rnd (apply next0 pick)
+        | (_, None) -> ()
+    }
+
+    let invent (superRows: int) (superColumns: int) (nextRandom: int -> int) =
+
+        let initialState () = Board.empty superRows superColumns |> SolverState.fromBoard
+
+        initialState ()
+        |> getHints nextRandom
+        |> Seq.fold apply (initialState ())
+        |> (fun state -> state.Board)
