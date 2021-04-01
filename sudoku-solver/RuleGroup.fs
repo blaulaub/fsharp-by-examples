ba1ff8a9dce0723b11c3c80ae8e813f734f4f9b8
Christian Fuchs <kontaktierbar@gmail.com>
2021-04-01 23:36:36 +0200

    make smaller

--- sudoku-solver/RuleGroup.fs
+++ sudoku-solver/RuleGroup.fs
@@ -7,21 +7,17 @@ type RuleGroup = (int*int) seq
 
 module RuleGroup =
 
-    /// <summary>
-    /// All groups where the Sudoku rule (unique value) should apply.
-    /// </summary>
-    /// <param name="superRows">number of sub-blocks per row</param>
-    /// <param name="superColumns">number of sub-blocks per column</param>
-    let groups (superRows: int) (superColumns: int): RuleGroup seq = seq {
-
-        let total = superRows * superColumns
-
+    let private rows total = seq {
         for row in 0..(total-1) ->
             seq { for col in 0..(total-1) -> (row, col) }
+    }
 
+    let private columns total = seq {
         for col in 0..(total-1) ->
             seq { for row in 0..(total-1) -> (row, col) }
+    }
 
+    let private blocks (superRows: int) (superColumns: int) = seq {
         let subRows = superColumns
         let subCols = superRows
         for supRow in 0..(superRows-1) do
@@ -31,4 +27,16 @@ module RuleGroup =
                 for subCol in 0..(subCols-1) ->
                     (supRow*superColumns+subRow, supCol*superRows+subCol)
             }
-        }
+    }
+
+    /// <summary>
+    /// All groups where the Sudoku rule (unique value) should apply.
+    /// </summary>
+    /// <param name="superRows">number of sub-blocks per row</param>
+    /// <param name="superColumns">number of sub-blocks per column</param>
+    let groups (superRows: int) (superColumns: int): RuleGroup seq = seq {
+        let total = superRows * superColumns
+        yield! rows total
+        yield! columns total
+        yield! blocks superRows superColumns
+    }
