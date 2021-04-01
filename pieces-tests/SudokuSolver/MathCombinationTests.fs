f54dc228079839b0f5baf1067a98caa88e261be9
Christian Fuchs <kontaktierbar@gmail.com>
2021-04-01 21:11:45 +0200

    add confidence tests

--- pieces-tests/SudokuSolver/MathCombinationTests.fs
+++ pieces-tests/SudokuSolver/MathCombinationTests.fs
@@ -7,8 +7,27 @@ open Ch.PatchCode.SudokuSolver
 let tests =
     testList "Sudoku MathCombination tests" [
 
-        test "verify options" {
-            ()
+        test "downTo depth zero of empty" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 0 [] |> Seq.toList) [] ""
         }
 
+        test "downTo depth zero of one" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 0 [1] |> Seq.toList) [] ""
+        }
+
+        test "downTo depth one of one" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 1 [1] |> Seq.toList) [ [1] ] ""
+        }
+
+        test "downTo depth one of two" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 1 [1; 2] |> Seq.toList) [ [1]; [2] ] ""
+        }
+
+        test "downTo depth two of two" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 2 [1; 2] |> Seq.toList) [ [1]; [2]; [1; 2] ] ""
+        }
+
+        test "downTo depth two of three" {
+            Expect.equal (MathCombinations.combinationsDownToDepth 2 [1; 2; 3] |> Seq.toList) [ [1]; [2]; [3]; [1; 2]; [1; 3]; [2; 3] ] ""
+        }
     ]
