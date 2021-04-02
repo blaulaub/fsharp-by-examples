aaf7cc77ff8dfde431f04245539581fe3fe02e17
Christian Fuchs <kontaktierbar@gmail.com>
2021-04-02 18:35:06 +0200

    test a bit more

--- pieces-tests/SudokuSolver/CrossGroupTests.fs
+++ pieces-tests/SudokuSolver/CrossGroupTests.fs
@@ -63,6 +63,24 @@ let tests =
             }) "first row"
         }
 
+        test "4 by 3 level 1 last corner row group" {
+            Expect.isTrue (CrossGroup.groupsAtLevel 4 3 1 |> groupsContain {
+                Level = 1
+                Intersection = seq { for col in 8..11 -> (11, col)}
+                Source = seq { for col in 0..7 -> (11, col)}
+                Target = seq { for row in 9..10 do for col in 8..11 -> (row, col)}
+            }) "last row"
+        }
+
+        test "4 by 3 level 1 last corner column group" {
+            Expect.isTrue (CrossGroup.groupsAtLevel 4 3 1 |> groupsContain {
+                Level = 1
+                Intersection = seq { for row in 9..11 -> (row, 11)}
+                Source = seq { for row in 0..8 -> (row, 11)}
+                Target = seq { for row in 9..11 do for col in 8..10 -> (row, col)}
+            }) "last column"
+        }
+
         test "4 by 4 level 1 corner group" {
             for x in CrossGroup.groupsAtLevel 4 4 1 do
                 printfn "l:%d i:%A s:%A t:%A" x.Level x.Intersection x.Source x.Target
