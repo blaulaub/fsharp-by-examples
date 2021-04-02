2134e5687c0b2cafdd3b9b00561caa211341730a
Christian Fuchs <kontaktierbar@gmail.com>
2021-04-02 17:44:47 +0200

    simplifying SingularCrossGroup

--- sudoku-solver/SingularCrossGroup.fs
+++ sudoku-solver/SingularCrossGroup.fs
@@ -21,17 +21,20 @@ module SingularCrossGroup =
                     Source = seq {
                         for subRow in 0..(superColumns-1) do
                         for col in 0..(total-1) do
-                        if (row%superColumns = subRow) && (col/superRows <> supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                        if (row%superColumns = subRow) && (col/superRows <> supCol)
+                        then yield (row, col)
                     }
                     Intersection = seq {
                         for subRow in 0..(superColumns-1) do
                         for col in 0..(total-1) do
-                        if (row%superColumns = subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                        if (row%superColumns = subRow) && (col/superRows = supCol)
+                        then yield (row, col)
                     }
                     Target = seq {
                         for col in 0..(total-1) do
                         for subRow in 0..(superColumns-1) do
-                        if (row%superColumns <> subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                        if (row%superColumns <> subRow) && (col/superRows = supCol)
+                        then yield ((row/superColumns)*superColumns+subRow, col)
                     }
                 }
 
@@ -41,17 +44,20 @@ module SingularCrossGroup =
                     Source = seq {
                         for subCol in 0..(superRows-1) do
                         for row in 0..(total-1) do
-                        if (col%superRows = subCol) && (row/superColumns <> supRow) then yield (row, (col/superRows)*superRows+subCol)
+                        if (col%superRows = subCol) && (row/superColumns <> supRow)
+                        then yield (row, col)
                     }
                     Intersection = seq {
                         for subCol in 0..(superRows-1) do
                         for row in 0..(total-1) do
-                        if (col%superRows = subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
+                        if (col%superRows = subCol) && (row/superColumns = supRow)
+                        then yield (row, col)
                     }
                     Target = seq {
                         for row in 0..(total-1) do
                         for subCol in 0..(superRows-1) do
-                        if (col%superRows <> subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
+                        if (col%superRows <> subCol) && (row/superColumns = supRow)
+                        then yield (row, (col/superRows)*superRows+subCol)
                     }
                 }
     }
