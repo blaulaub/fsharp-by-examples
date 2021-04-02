a95e05f7c46c442e85cbf24039643910ecdb8b56
Christian Fuchs <kontaktierbar@gmail.com>
2021-04-02 14:25:06 +0200

    fix loop bug

--- sudoku-solver/CrossGroup.fs
+++ sudoku-solver/CrossGroup.fs
@@ -17,42 +17,42 @@ module CrossGroup =
         let total = superRows * superColumns
 
         for row in 0..(total-1) do
-        for supCol in 0..(superColumns-1) do
-        yield {
-            Source = seq {
-                for subRow in 0..(superColumns-1) do
-                for col in 0..(total-1) do
-                if (row%superColumns = subRow) && (col/superRows <> supCol) then yield ((row/superColumns)*superColumns+subRow, col)
-            }
-            Intersection = seq {
-                for subRow in 0..(superColumns-1) do
-                for col in 0..(total-1) do
-                if (row%superColumns = subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
-            }
-            Target = seq {
-                for col in 0..(total-1) do
-                for subRow in 0..(superColumns-1) do
-                if (row%superColumns <> subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
-            }
-        }
+            for supCol in 0..(superColumns-1) do
+                yield {
+                    Source = seq {
+                        for subRow in 0..(superColumns-1) do
+                        for col in 0..(total-1) do
+                        if (row%superColumns = subRow) && (col/superRows <> supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                    }
+                    Intersection = seq {
+                        for subRow in 0..(superColumns-1) do
+                        for col in 0..(total-1) do
+                        if (row%superColumns = subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                    }
+                    Target = seq {
+                        for col in 0..(total-1) do
+                        for subRow in 0..(superColumns-1) do
+                        if (row%superColumns <> subRow) && (col/superRows = supCol) then yield ((row/superColumns)*superColumns+subRow, col)
+                    }
+                }
 
         for col in 0..(total-1) do
-        for supRow in 0..(superRows-1) do
-        yield {
-            Source = seq {
-                for subCol in 0..(superRows-1) do
-                for row in 0..(total-1) do
-                if (col%superRows = subCol) && (row/superColumns <> supRow) then yield (row, (col/superRows)*superRows+subCol)
-            }
-            Intersection = seq {
-                for subCol in 0..(superRows-1) do
-                for row in 0..(total-1) do
-                if (col%superRows = subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
-            }
-            Target = seq {
-                for row in 0..(total-1) do
-                for subCol in 0..(superRows-1) do
-                if (col%superRows <> subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
-            }
-        }
+            for supRow in 0..(superRows-1) do
+                yield {
+                    Source = seq {
+                        for subCol in 0..(superRows-1) do
+                        for row in 0..(total-1) do
+                        if (col%superRows = subCol) && (row/superColumns <> supRow) then yield (row, (col/superRows)*superRows+subCol)
+                    }
+                    Intersection = seq {
+                        for subCol in 0..(superRows-1) do
+                        for row in 0..(total-1) do
+                        if (col%superRows = subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
+                    }
+                    Target = seq {
+                        for row in 0..(total-1) do
+                        for subCol in 0..(superRows-1) do
+                        if (col%superRows <> subCol) && (row/superColumns = supRow) then yield (row, (col/superRows)*superRows+subCol)
+                    }
+                }
     }
